1bead1be3c22066cc2286772ac909e9c6a1f57c6
Keren WuRohe <kerenrohe@gmail.com>
2018-08-26 21:52:38 -0400

    Grammable - adjusted migration files

--- db/migrate/20180820024322_remove_pictures_from_gram.rb
+++
@@ -1,5 +0,0 @@
-class RemovePicturesFromGram < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :grams, :pictures, :json
-  end
-end
--- db/migrate/20180820025054_remove_picture_from_gram.rb
+++
@@ -1,5 +0,0 @@
-class RemovePictureFromGram < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :grams, :picture, :json
-  end
-end
